4dbf3589e4c507512fcfe5c6f7b93865c56fa48e
shindesandy <114210879+shindesandy@users.noreply.github.com>
2022-09-23 12:08:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+resource "aws_instance" "myawsserver" {
+  ami = var.image
+  instance_type = "t2.nano"
+ # key_name = "sandeep-import"
+  tags = {
+    Name = lower (var.name)
+    env = upper (var.env)
+    owner = upper ("Techlanders")
+    dept = "Eng"
+    client = "vodafone"
+  }
+}
